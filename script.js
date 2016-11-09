ab2fabc3a9fced59a61b5c5699bd9c85e5063871
Yaniv Izaki <Yaniv Izaki>
2016-11-09 14:51:27 +0200

    .

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var beers = [];
+
 var addBeer = function(name, category) {
 	var beer = new Object();
 	beer.name = name;
@@ -5,42 +7,16 @@ var addBeer = function(name, category) {
 	beers.push(beer);
 };
 
+var updateBeers = function () {
+	$('.beers-list').empty();
+	for (var i = 0; i < beers.length; i++) {
+		$('.beers-list').append('<li>'+beers[i].name+"-"+beers[i].category+'</li>');
+	};
+};
+
 $('.post-beer').on('click', function () {
 	var name = $(".beer-input").val();
 	var category = $(".category-input").val();
 	addBeer(name,category);
-
+	updateBeers();
 });
-
-var beers = [];
-
-
-
-
-// var beers = []
-
-// var updateBeers = function () {
-// 	$('.beers-list').empty();
- 
-// 	for (var i = 0; i < beers.length; i++) {
-// 		var beerHTML = "<li>" + beers[i].name /
-// 		+ "-"	+ beers[i].category+"</li>";
-// 		$('.beers-list').append(beerHTML);
-// 	}
-// }
-
-// var addBeer = function (beerName,beerCategory) {
-// 	var newBeer = {
-// 		name: beerName,
-// 		category: beerCategory
-// 	};
-
-// 	beers.push(newBeer)
-// };
-
-// $('post-beer').on('click', function (event) {
-// 	event.preventDefault();
-// 	var name = $('.beer-input').val();
-// 	var category = $('category-input').val();
-// 	addBeer(name,category);
-// });
